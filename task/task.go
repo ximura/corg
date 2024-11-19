e39d17f23e135277d1841ee8cfde00ed9aba61ef
Oleh Rukin <3680312+ximura@users.noreply.github.com>
2024-11-19 21:46:21 +0200

    simple task description

--- task/task.go
+++ task/task.go
@@ -1,1 +1,40 @@
 package task
+
+import (
+	"time"
+
+	"github.com/docker/go-connections/nat"
+	"github.com/google/uuid"
+)
+
+type State int
+
+const (
+	Pending State = iota
+	Scheduled
+	Running
+	Completed
+	Failed
+)
+
+// Task - orchestrator task description
+type Task struct {
+	ID            uuid.UUID
+	Name          string
+	State         State
+	Image         string
+	Memory        int
+	Disk          int
+	ExposedPorts  nat.PortSet
+	RestartPolicy string
+	StartTime     time.Time
+	FinishTime    time.Time
+}
+
+// TaskEvent - event that used by task system for state transition
+type TaskEvent struct {
+	ID        uuid.UUID
+	State     State
+	Timestamp time.Time
+	Task      Task
+}
