aeeae704a3d7f4bdb2fa638109946541624f7608
Oleh Rukin <3680312+ximura@users.noreply.github.com>
2024-11-19 21:52:47 +0200

    worker stub

--- worker/worker.go
+++ worker/worker.go
@@ -1,1 +1,32 @@
 package worker
+
+import (
+	"fmt"
+
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+	"github.com/ximura/corg/task"
+)
+
+type Worker struct {
+	Name      string
+	Queue     queue.Queue
+	Db        map[uuid.UUID]*task.Task
+	TaskCount int
+}
+
+func (w *Worker) CollectStats() {
+	fmt.Println("I will collect stats")
+}
+
+func (w *Worker) RunTask() {
+	fmt.Println("I will start or stop a task")
+}
+
+func (w *Worker) StartTask() {
+	fmt.Println("I will start a task")
+}
+
+func (w *Worker) StopTask() {
+	fmt.Println("I will stop a task")
+}
